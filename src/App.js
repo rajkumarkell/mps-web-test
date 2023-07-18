fa0fca4277796e1364c13e6b44b598d27d3c6182
rajkumarkell <rajkumar.kell@gmail.com>
2023-07-18 15:49:20 +0100

    Added response forma

--- src/App.js
+++ src/App.js
@@ -19,8 +19,48 @@ function App() {
           API to use: GET:
           `https://preprodapi.mypatientspace.com/api/artifact/libraries?page=0&size=20`
         </div>
-        <div style={{marginTop:20}}>
-          Access Token to use: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsidGVzdGp3dHJlc291cmNlaWQiXSwidXNlcl9uYW1lIjoidGVzdGFkbWluIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9GQUNJTElUWV9BRE1JTiJdLCJqdGkiOiJiNDY0NWIzYi02ZjA5LTQ2YjYtOTAzMy0wOTRiMzU3YTc5MGMiLCJjbGllbnRfaWQiOiJ0ZXN0and0Y2xpZW50aWQiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXX0.Wuw785LT5wmoc5P7_pCQ8PSwPAPJCcKJ-WG2S6hqYdQ
+        <div style={{ marginTop: 20 }}>
+          This API uses Bearer authentication. Access Token to use:
+          <p>
+            <pre>
+              <code
+                style={{ whiteSpace: "break-spaces", wordBreak: "break-all" }}
+              >
+                eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsidGVzdGp3dHJlc291cmNlaWQiXSwidXNlcl9uYW1lIjoidGVzdGFkbWluIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9GQUNJTElUWV9BRE1JTiJdLCJqdGkiOiJiNDY0NWIzYi02ZjA5LTQ2YjYtOTAzMy0wOTRiMzU3YTc5MGMiLCJjbGllbnRfaWQiOiJ0ZXN0and0Y2xpZW50aWQiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXX0.Wuw785LT5wmoc5P7_pCQ8PSwPAPJCcKJ-WG2S6hqYdQ
+              </code>
+            </pre>
+          </p>
+        </div>
+        <div style={{ marginTop: 20 }}>
+          Response Format:
+          <p>
+            <pre>
+              <code>
+                {`{
+              "_embedded": {
+                  "libraries": [
+                          {
+                            "id": "63feee5a38fd171d656d122f",
+                            "createdOn": 1624490671561,
+                            "lastUpdatedOn": 1688558492869,
+                            "name": "Vulvovaginal Health",
+                            "title": "Vulvovaginal Health",
+                            "status": "ACTIVE"
+                          },
+                          {
+                            "id": "64a2dd0b46d101338d31a4dd",
+                            "createdOn": 1688395019035,
+                            "lastUpdatedOn": 1688555751537,
+                            "name": "Happy Stars Lottie",
+                            "title": "Happy Stars Lottie",
+                            "status": "ACTIVE"
+                          }
+                  ]
+            }
+            }`}
+              </code>
+            </pre>
+          </p>
         </div>
       </div>
     </div>
